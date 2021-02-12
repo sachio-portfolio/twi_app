fb1ed5b41845acd29f3241806558356ea220df8d
sachio-portfolio <sachio.engineer@gmail.com>
2021-02-12 12:18:51 +0900

    :octocat: Add
    First commit

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+ActiveRecord::Schema.define(version: 2021_02_10_133749) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
