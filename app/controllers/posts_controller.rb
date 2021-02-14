66891b28bf71c4f7253014528cdce009e6ea07d8
sachio-portfolio <sachio.engineer@gmail.com>
2021-02-14 16:17:34 +0900

    :sparkles: Add 編集機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
+  before_action :select_post, only: [:edit, :update]
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
   def new
     @post = Post.new
@@ -14,8 +15,21 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  def edit
+  end
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path,
+      notice: "あなたのぼやきを編集しました！"
+    else
+      render :edit
+    end
+  end
   private
   def post_params
     params.require(:post).permit(:content)
+  end
+  def select_post
+    @post = Post.find(params[:id])
   end
 end
